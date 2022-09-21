1e90a65db5c84cebcd298d26746e45e04aa74ab3
App Generator <51070104+app-generator@users.noreply.github.com>
2022-09-21 09:51:26 +0300

    API GEN - Added Models File

---
+++ apps/models.py
@@ -0,0 +1,17 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from apps import db
+
+'''
+Add your models below
+'''
+
+'''
+# Book Sample
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64))
+'''
