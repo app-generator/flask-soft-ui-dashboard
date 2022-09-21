9dc4fb553fc27c1055ba0ac9ba05a6441529a52e
App Generator <51070104+app-generator@users.noreply.github.com>
2022-09-21 09:53:11 +0300

    API GEN - Register API BP

--- apps/__init__.py
+++ apps/__init__.py
@@ -19,7 +19,7 @@ def register_extensions(app):
 
 
 def register_blueprints(app):
-    for module_name in ('authentication', 'home'):
+    for module_name in ('authentication', 'home', 'api'):
         module = import_module('apps.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
